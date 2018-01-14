b9fa00be2c6591f4b4897a36a292774ea4493391
a.monemian@gmail.com <Tehran12>
2018-01-14 13:46:05 +0330

    add mp3 convertor debug

--- public/mp3Worker.js
+++ public/mp3Worker.js
@@ -5,10 +5,18 @@ var mp3codec;
 self.onmessage = function(e) {
 	switch (e.data.cmd) {
 	case 'init':
+		alert("ummad mp3Worker init!!!!!!");
+
 		if (!e.data.config) {
 			e.data.config = { };
 		}
+		alert("ghabl az lame.init()");
+
 		mp3codec = Lame.init();
+
+		alert("bad az lame.init()");
+		alert("mp3 Codec: " + mp3codec);
+
 
 		Lame.set_mode(mp3codec, e.data.config.mode || Lame.JOINT_STEREO);
 		Lame.set_num_channels(mp3codec, e.data.config.channels || 2);
@@ -17,8 +25,23 @@ self.onmessage = function(e) {
 		Lame.set_out_samplerate(mp3codec, e.data.config.samplerate || 44100);
 		Lame.set_bitrate(mp3codec, e.data.config.bitrate || 128);
 
+		alert("Params set!@");
+
+
 		Lame.init_params(mp3codec);
+		alert("initiated");
+
 		console.log('Version :', Lame.get_version() + ' / ',
+			'Mode: '+Lame.get_mode(mp3codec) + ' / ',
+			'Samples: '+Lame.get_num_samples(mp3codec) + ' / ',
+			'Channels: '+Lame.get_num_channels(mp3codec) + ' / ',
+			'Input Samplate: '+ Lame.get_in_samplerate(mp3codec) + ' / ',
+			'Output Samplate: '+ Lame.get_in_samplerate(mp3codec) + ' / ',
+			'Bitlate :' +Lame.get_bitrate(mp3codec) + ' / ',
+			'VBR :' + Lame.get_VBR(mp3codec));
+
+
+		alert('Version :', Lame.get_version() + ' / ',
 			'Mode: '+Lame.get_mode(mp3codec) + ' / ',
 			'Samples: '+Lame.get_num_samples(mp3codec) + ' / ',
 			'Channels: '+Lame.get_num_channels(mp3codec) + ' / ',
