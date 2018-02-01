82db81e3af42832767081661c255e13ce8d5cd90
a.monemian@gmail.com <Tehran12>
2018-02-01 18:12:55 +0330

    Powered by footer

--- packages/rocketchat-integrations/server/lib/triggerHandler.js
+++ packages/rocketchat-integrations/server/lib/triggerHandler.js
@@ -392,9 +392,8 @@ RocketChat.integrations.triggerHandler = new class RocketChatIntegrationHandler 
 				data.user_id = message.u._id;
 				data.user_name = message.u.username;
 				data.text = message.msg ;
-
 				data.message = message;
-
+				data.usersTo.username = room.usersTo.username;
 
 				if (message.alias) {
 					data.alias = message.alias;
@@ -419,6 +418,7 @@ RocketChat.integrations.triggerHandler = new class RocketChatIntegrationHandler 
 				data.user = user;
 				data.room = room;
 				data.message = message;
+				data.usersTo.username = room.usersTo.username;
 
 
 				if (message.alias) {
