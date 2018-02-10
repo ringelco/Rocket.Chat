29675cc251fc0b06e42e568be6c355b234636671
a.monemian@gmail.com <Tehran12>
2018-02-10 18:05:54 +0330

    mp3 converter MB version

--- packages/rocketchat-ui/client/lib/recorderjs/recorder.js
+++ packages/rocketchat-ui/client/lib/recorderjs/recorder.js
@@ -63,13 +63,13 @@
 		}
 
 		this.exportWAV = function(cb, type){
-			/*currCallback = cb || config.callback;
+			currCallback = cb || config.callback;
 			type = type || config.type || 'audio/wav';
 			if (!currCallback) throw new Error('Callback not set');
 			worker.postMessage({
 				command: 'exportWAV',
 				type: type
-			});*/
+			});
 		}
 
 		//Mp3 conversion
@@ -99,12 +99,13 @@
 						var mp3Blob = new Blob([new Uint8Array(e.data.buf)], {
 							type: 'audio/mp3'
 						});
+						fileReader.readAsArrayBuffer(mp3Blob);
 						currCallback(mp3Blob);
 					}
 				};
 			};
-			fileReader.readAsArrayBuffer(blob);
-			currCallback(blob);
+			// fileReader.readAsArrayBuffer(blob);
+			// currCallback(blob);
 		}
 
 
