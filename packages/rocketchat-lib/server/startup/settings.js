0fa286781033583312dc7682063870f9dfe05546
a.monemian@gmail.com <Tehran12>
2018-03-11 14:08:52 +0330

    Persian character for naming

--- packages/rocketchat-lib/server/startup/settings.js
+++ packages/rocketchat-lib/server/startup/settings.js
@@ -422,6 +422,7 @@ RocketChat.settings.addGroup('General', function() {
 		type: 'action',
 		actionText: 'Restart_the_server'
 	});
+
 	this.section('UTF8', function() {
 		this.add('UTF8_Names_Validation', '[0-9a-zA-Z-_.\u0600-\u06FF\u0698\u067E\u0686\u06AF]+', {
 			type: 'string',
